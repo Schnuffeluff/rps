fff538ec67c619ad464bf1a12329c478e79ae9a2
Schnuffeluff <127944896+Schnuffeluff@users.noreply.github.com>
2025-03-12 21:16:28 +0100

    Created onetime playthrough.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice () {
     let choises = ["rock","paper","scissors"];
@@ -7,4 +7,53 @@ function getComputerChoice () {
     
 }
 
-console.log(getComputerChoice());
+function getHumanChoice () {
+    return prompt("Choose your weapon!").toLowerCase();
+    
+}
+
+function playRound (humanChoice, computerChoice) {
+    
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log ("You lose! "+computerChoice+" beats"+humanChoice+"!")
+        computerScore++;
+    }
+
+    if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log ("You lose! "+computerChoice+" beats"+humanChoice+"!")
+        computerScore++;
+    }
+
+    if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log ("You lose! "+computerChoice+" beats"+humanChoice+"!")
+        computerScore++;
+    }
+
+    if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log ("You win! "+humanChoice+" beats"+computerChoice+"!")
+        humanScore++;
+    }
+
+    if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log ("You win! "+humanChoice+" beats"+computerChoice+"!")
+        humanScore++;
+    }
+
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log ("You win! "+humanChoice+" beats"+computerChoice+"!")
+        humanScore++;
+    }
+
+    else {
+        console.log("Draw! You both chose " + humanSelection +"!")
+    }
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
+
+
+
