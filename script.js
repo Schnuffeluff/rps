b2d8576a6a7e364e0e028f3a1beddc47416bb6a5
Schnuffeluff <127944896+Schnuffeluff@users.noreply.github.com>
2025-03-22 13:25:12 +0100

    created dom methods for result and score

--- script.js
+++ script.js
@@ -9,49 +9,86 @@ function getHumanChoice () {
     
 }
 
+
+let humanScore = 0;
+let computerScore = 0;
+let result = document.querySelector("#results");
+let text = document.createElement("p");
+let score = document.querySelector("#score");
+let num = document.createElement("h3");
+
+num.textContent = "Humanscore:"+humanScore+" Computerscore:"+computerScore;
+score.appendChild(num);
+
 function playRound (humanChoice) {
 
     const computerChoice = getComputerChoice();
+    
+    
 
     if (humanChoice == "rock" && computerChoice == "paper") {
         console.log ("You lose! "+computerChoice+" beats "+humanChoice+"!")
         computerScore++;
+        text.textContent = "You lose! "+computerChoice+" beats "+humanChoice+"!";
+        result.appendChild(text);
     }
 
     if (humanChoice == "paper" && computerChoice == "scissors") {
         console.log ("You lose! "+computerChoice+" beats "+humanChoice+"!")
         computerScore++;
+        text.textContent = "You lose! "+computerChoice+" beats "+humanChoice+"!";
+        result.appendChild(text);
     }
 
     if (humanChoice == "scissors" && computerChoice == "rock") {
         console.log ("You lose! "+computerChoice+" beats "+humanChoice+"!")
         computerScore++;
+        text.textContent = "You lose! "+computerChoice+" beats "+humanChoice+"!";
+        result.appendChild(text);
     }
 
     if (humanChoice == "scissors" && computerChoice == "paper") {
         console.log ("You win! "+humanChoice+" beats "+computerChoice+"!")
         humanScore++;
+        text.textContent = "You win! "+humanChoice+" beats "+computerChoice+"!";
+        result.appendChild(text);
     }
 
     if (humanChoice == "paper" && computerChoice == "rock") {
         console.log ("You win! "+humanChoice+" beats "+computerChoice+"!")
         humanScore++;
+        text.textContent = "You win! "+humanChoice+" beats "+computerChoice+"!";
+        result.appendChild(text);
     }
 
     if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log ("You win! "+humanChoice+" beats "+computerChoice+"!")
         humanScore++;
+        text.textContent = "You win! "+humanChoice+" beats "+computerChoice+"!";
+        result.appendChild(text);
     }
 
     if (humanChoice == computerChoice) {
         console.log("Draw! You both chose " + humanChoice +"!")
+        text.textContent = "Draw! You both chose " + humanChoice +"!";
+        result.appendChild(text);
     }
-      
+     
+    if (humanScore == 5) {
+        text.textContent = "You have won!";
+        result.appendChild(text);
+    }
+
+    if (computerScore == 5) {
+        text.textContent = "Computer has won!";
+        result.appendChild(text);
+    }
+
+    num.textContent = "Humanscore:"+humanScore+" Computerscore:"+computerScore;
+    score.appendChild(num);
+
 }
 
-let humanScore = 0;
-let computerScore = 0;
- 
 
 
 const paper = document.querySelector("#paper");
